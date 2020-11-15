34bc0a6c244296db4444261166065766eefd8077
frcampos <42178315+frcampos@users.noreply.github.com>
2020-11-15 14:51:45 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,43 +1,57 @@
-radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
+radio.onReceivedNumber(function (receivedNumber) {
     radio.setGroup(1)
     basic.showNumber(receivedNumber)
 })
-//  Transmite Passos para o outro microbit
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+// Transmite Passos para o outro microbit
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("P")
     radio.sendNumber(passosdados)
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    radio.sendNumber(temperaturaMaximaregistada)
-})
-input.onGesture(Gesture.Shake, function on_gesture_shake() {
-    
+input.onGesture(Gesture.Shake, function () {
     passos = passosdados
     passos += 1
     passosdados = passos
     passosdados += passosdados
     basic.showNumber(passosdados)
 })
-//  Quando se pressiona reinicia contagem de passos
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
-    
+// Quando se pressiona reinicia contagem de passos
+input.onButtonPressed(Button.AB, function () {
     passosdados = 0
     passos = 0
 })
-//  Definido grupo de radio para 1 de forma a sincronizar apenas com esse grupo Radio.
-//  Inicialização de variáveis globais.
-//  Visualização de símbolo para verificação inicial.
-let temperaturaMaximaregistada = -5
-let temperaturaReal = -5
+radio.onReceivedString(function (receivedString) {
+    radio.setGroup(1)
+    if (receivedString == "P") {
+        basic.showString("Passos")
+        basic.pause(500)
+        basic.showNumber(passosdados)
+        basic.pause(2000)
+    } else {
+        basic.showString("Temperatura")
+        basic.pause(100)
+        basic.showNumber(temperaturaMaximaregistada)
+        basic.pause(2000)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("T")
+    radio.sendNumber(temperaturaMaximaregistada)
+})
 let passos = 0
 let passosdados = 0
+let temperaturaMaximaregistada = 0
+// Definido grupo de radio para 1 de forma a sincronizar apenas com esse grupo Radio.
+// Inicialização de variáveis globais.
+// Visualização de símbolo para verificação inicial.
+temperaturaMaximaregistada = -5
+let temperaturaReal = -5
 radio.setGroup(1)
 passosdados = 0
 passos = 0
 basic.showIcon(IconNames.Yes)
 basic.pause(1000)
 basic.clearScreen()
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
     temperaturaReal = input.temperature()
     basic.showNumber(temperaturaReal)
     temperaturaMaximaregistada = Math.max(temperaturaMaximaregistada, temperaturaReal)
